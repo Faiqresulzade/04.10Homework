064cbf1adc89228ace55a7d2c7c2c5724d147f54
Faiqresulzade <faigbr@code.edu.az>
2022-10-04 17:32:53 +0400

    Proje dosyası ekle.

---
+++ 04.10Homework/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _04._10Homework.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Name = "Faiq";
+            ViewBag.Surname = "Resulzade";
+            ViewBag.Age = 20;
+            ViewBag.Country = "Azerbaijan";
+            ViewBag.Phone_Number = "+994 ** *** ** **";
+            return View();
+        }
+    }
+}
